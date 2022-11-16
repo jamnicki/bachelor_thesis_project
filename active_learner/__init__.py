068e0ac277b0f619ac873b4772427c4d7c0770d7
Jędrzej Jamnicki <jedrzej.jamnicki@gmail.com>
2022-11-16 19:19:08 +0100

    Add base of function aproached query strategies

---
+++ active_learner/__init__.py
@@ -0,0 +1,6 @@
+from .query_strategy import query_random, query_least_confidence
+
+__all__ = [
+    "query_random",
+    "query_least_confidence"
+]
---
+++ active_learner/query_strategy/__init__.py
@@ -0,0 +1,7 @@
+from .random_strategy import query_random
+from .least_confidence_strategy import query_least_confidence
+
+__all__ = [
+    "query_random",
+    "query_least_confidence"
+]
