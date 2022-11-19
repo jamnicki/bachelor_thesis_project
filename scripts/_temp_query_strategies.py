f382d85aedf014a12dfbfb4de5241c8ddf961a4a
Jędrzej Jamnicki <jedrzej.jamnicki@gmail.com>
2022-11-19 02:01:43 +0100

    Refactor, perfomance improved 2 times :O

--- scripts/_temp_query_strategies.py
+++ scripts/_temp_query_strategies.py
@@ -25,18 +25,23 @@ def query_least_confidence(nlp, included_components, examples,
         scores = np.repeat(2.0, ex_len)  # 2 means unscored, 0 < score < 1
         # idx < 0 means unscored
         scores_idxs = np.linspace(-1, 0, ex_len, endpoint=False)
-        for i in range(ex_len):
-            if i in exclude:
-                continue
-            example = examples[i]
-            with nlp.select_pipes(enable=included_components):
-                pred = nlp(example.text)
+        texts = [
+            example.text
+            for i, example in enumerate(examples)
+            if i not in exclude
+        ]
+        disabled_comps = set(nlp.pipe_names) - set(included_components)
+        predictions = nlp.pipe(texts, disable=disabled_comps)
+        for i, pred in enumerate(predictions):
             spans = pred.spans[spans_key]
             if spans:
                 scores[i] = spans.attrs["scores"].mean()
                 scores_idxs[i] = i
         non_zero_score_argmin = np.argmin(scores[np.nonzero(scores)])
         return int(scores_idxs[non_zero_score_argmin])
+
+    if isinstance(included_components, str):
+        included_components = [included_components]
 
     n_queried = 0
     ex_len = len(examples)
