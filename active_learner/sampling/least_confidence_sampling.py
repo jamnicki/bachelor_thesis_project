8ad4f7b52f14f2fddf8bde6115191af3af873ef6
Jędrzej Jamnicki <jedrzej.jamnicki@gmail.com>
2022-11-21 00:31:54 +0100

    Update fixed least-cofidence sampling

--- active_learner/sampling/least_confidence_sampling.py
+++ active_learner/sampling/least_confidence_sampling.py
@@ -8,31 +8,36 @@ def query_least_confidence(nlp, included_components, examples,
     SpanCategorizer. Based on mean score of all labels for each span.
     """
     def _get_least_confident():
-        """Calculate mean score of all labels for single span."""
-        scores = np.repeat(2.0, ex_len)  # 2 means unscored, 0 < score < 1
-        # idx < 0 means unscored
-        scores_idxs = np.linspace(-1, 0, ex_len, endpoint=False)
-        texts = [
+        for _idx in indexes_by_score:
+            include = include_arr[_idx]
+            if include:
+                return _idx
+
+    disabled_comps = set(nlp.pipe_names) - set(included_components)
+    texts = [
             example.text
             for i, example in enumerate(examples)
             if i not in exclude
         ]
-        cpus = os.cpu_count()
-        predictions = nlp.pipe(texts, disable=disabled_comps, n_process=cpus)
-        for i, pred in enumerate(predictions):
-            spans = pred.spans[spans_key]
-            if spans:
-                scores[i] = spans.attrs["scores"].mean()
-                scores_idxs[i] = i
-        non_zero_score_argmin = np.argmin(scores[np.nonzero(scores)])
-        return int(scores_idxs[non_zero_score_argmin])
+    n_process = os.cpu_count()
+    predictions = nlp.pipe(texts, disable=disabled_comps, n_process=n_process)
 
-    disabled_comps = set(nlp.pipe_names) - set(included_components)
-    n_queried = 0
     ex_len = len(examples)
+    include_arr = np.repeat(True, ex_len)
+    for ex in exclude:
+        include_arr[ex] = False
+
+    scores = np.zeros(ex_len)
+    for i, pred in enumerate(predictions):
+        spans = pred.spans[spans_key]
+        if spans:
+            scores[i] = spans.attrs["scores"].mean()
+    indexes_by_score = np.argsort(scores)  # ascending order
+
+    n_queried = 0
     while n_queried < n_instances:
         idx = _get_least_confident()
-        exclude.add(idx)
+        include_arr[idx] = False
         example = examples[idx]
         n_queried += 1
         yield idx, example
